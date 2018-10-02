4cd76c16895568b3fee3b91fbf0d642926e30bd5
kymry burwell <kymry.burwell@gmail.com>
2018-10-02 17:35:37 +0200

    Added poisson distribution function

--- 02/lab2.r
+++ 02/lab2.r
@@ -1,9 +1,12 @@
-install.packages("expss")
-install.packages('knitr')
-install.packages('data.table')
-library(expss)
-library(knitr)
-library(data.table)
+requiredPackages <- c("igraph", "ggplot2", "data.table", "expss", "stats4", "VGAM")
+for (pac in requiredPackages) {
+  if(!require(pac,  character.only=TRUE)){
+    install.packages(pac, repos="http://cran.rstudio.com")
+    library(pac,  character.only=TRUE)
+  } 
+}
+rm(pac)
+rm(requiredPackages)
 
 # Set WD and load data
 setwd("~/Google Drive/UPC/Fall 2018/CSN/Labs/Lab2")
@@ -47,4 +50,51 @@ degree_sequence = read.table("./data/English_out-degree_sequence.txt", header = 
 degree_spectrum = table(degree_sequence)
 barplot(degree_spectrum, main = "English", xlab = "degree", ylab = "number of vertices")
 barplot(degree_spectrum, main = "English", xlab = "degree", ylab = "number of vertices", log = "xy")
+
+# Variables to be used in distribution functions
+get_MP <- function(x) { return(sum(log(x)))}
+get_C <- function(x) { C = 0
+                       for(i in x){
+                           j = 2
+                           while( j <= i){
+                             C <- C + log(j)
+                             j = j+1
+                           }
+                         } 
+                        return(C)
+                      }
+
+# Estimating parameters with log-likelihood (Maximum likelihood estimation)
+# TODO
+
+
+# Minus log-likelihood function (minus_log_likelihood = -L)
+minus_log_likelihood_zeta <- function(gamma){ 
+  length(x) * log(zeta(gamma)) + gamma * sum(log(x))
+}
+
+mle_zeta <- mle(minus_log_likelihood_zeta, 
+                start = list(gamma = 2),
+                method = "L-BFGS-B",
+                lower = c(1.00000001))
+summary(mle_zeta)
+attributes(summary(mle_zeta))
+attributes(summary(mle_zeta))$coef[1]
+
+
+# Displaced Poisson function
+x <- degree_sequence$V1  
+lambda_0 = sum(x)/length(x) # Strating Lambda for trial 
+poisson <- function(lambda){
+  -(sum(x) * log(lambda) - length(x)*(lambda + log(1 - exp(-lambda))) - get_C(x))
+}
+poisson(lambda_0)
+
+mle_poisson <- mle(poisson, 
+                start = list(lambda = lambda_0),
+                method = "L-BFGS-B",
+                lower = c(1.00000001))
+summary(mle_poisson)
+attributes(summary(mle_poisson))
+attributes(summary(mle_poisson))$coef[1]
 
