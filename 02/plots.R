be7f796225b36cdd95e159011a0948043a3c60b2
kymry burwell <kymry.burwell@gmail.com>
2018-10-16 14:21:42 +0200

    added altmann plots

--- 02/plots.R
+++ 02/plots.R
@@ -1,5 +1,5 @@
 
-#degree_sequence = read.table("./data/English_out-degree_sequence.txt", header = FALSE)
+degree_sequence = read.table("./data/English_out-degree_sequence.txt", header = FALSE)
 
 ######## Plotting Data vs Geometric ######## 
 
@@ -68,8 +68,35 @@ zeta_plot <- function(degree_sequence, maxdegree, gamma, language){
 }
 
 
+######## Plotting Data vs Altmann ######## 
+
+a_dist <- function(x, gamma, delta, N){
+  c <- 1/sum(seq(1:N)^(-gamma)*exp(-delta*seq(1:N)))
+  
+  return( c * x^(-gamma) * exp(-delta * x) )
+}
 
 
-
-
+altmann_plot <- function(degree_sequence, gamma, delta, language){
+  # Prepare data frame
+  N = length(degree_sequence$V1) # number of nodes in network
+  df <- data.frame(table(degree_sequence)) # data frame to store data for plotting
+  df$degree_sequence <- as.numeric(as.character(df$degree_sequence)) # change to numeric so can plot
+  df$gFreq <- df$degree_sequence # add column for distribution column
+  df[3] <- apply(df[3], 2, a_dist, as.numeric(gamma), as.numeric(delta), as.numeric(N)) # apply(data_frame,1,function,arguments_to_function_if_any)
+  df[2] <- df[2]/N # normalize data to show probability
+  max_p <- max(df[2]) # largest probability in network
+  min_p <- min(df[2]) # smallest probability in network
+  
+  # Plot the data
+  ggplot(df, aes(degree_sequence)) + 
+    geom_point( aes(y=Freq), colour="blue") +  # first layer is point graph
+    geom_line( aes(y=gFreq), colour = "green") + # second layer is a line graph
+    scale_x_continuous(trans='log10', limits=(c(1,10000))) + # sets x-axis to log10 scale
+    scale_y_continuous(trans='log10', limits=c(min_p, max_p+.02)) +   # sets y-axis to log10 scale
+    ylab("Probability") + 
+    xlab("Degree") + 
+    labs(title=paste("Altmann distribution with", language, sep= " "))+
+    theme(plot.title = element_text(hjust = 0.5))
+}
 
